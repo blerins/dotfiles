349bf70ab4f01713b7d322e03105856e36f8cefa
blerins <blerins@gmail.com>
2020-06-24 23:24:12 +0200

    Create Microsoft.PowerShell_profile.ps1

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,12 @@
+# C:\Users\<user>\Documents\WindowsPowerShell\
+function prompt {
+  Write-Host "PS " -NoNewline
+  Write-Host $(get-location) -NoNewline -ForegroundColor "darkCyan"
+  $status = git status
+    if($status -ne $NULL) {
+      Write-Host " [GIT:" -NoNewline -ForegroundColor "green"
+      Write-Host $status.Split("`r")[0].Replace("On branch ", "") -NoNewline -ForegroundColor "green"
+      Write-Host "]" -NoNewline -ForegroundColor "green"
+    }
+  return "> "
+}
